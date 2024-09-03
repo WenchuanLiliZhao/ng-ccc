801148c2d8c305905bfa2eebc05147cd545aa8fb
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2024-09-03 23:27:28 +0800

    Update Post_6.tsx

--- src/Pages/Posts/Stories/Post_6.tsx
+++ src/Pages/Posts/Stories/Post_6.tsx
@@ -12,8 +12,8 @@ const Post_6: Template_Page = {
       "我們希望，通過這種方式，不僅能夠讓大家了解到有關大熊貓的一些生物學知識，更能夠了解到，生態環境之於人類的重要性，以及（同樣重要的）：大熊貓何以能夠成為各國之間和平的使者。要知道，我們的世界充滿了衝突，其中有一些確實可以並且曾經被可愛所感化的；但何以感化？這也是我們在主題展中所說的。",
     latest_update: [2024, 9, 1],
     cover:
-      "https://i.pinimg.com/1200x/3e/28/81/3e2881cbd62c055013e0a3a6cb22d53a.jpg",
-    label: undefined,
+      "https://i.pinimg.com/1200x/3b/ca/29/3bca29a3d839c76e4010ea5917cb03fd.jpg",
+    label: "9月2日至12月31日",
     front: undefined,
     showHeader: undefined,
     title_display: undefined,
@@ -31,7 +31,7 @@ const Post_6: Template_Page = {
           art={
             <>
               <img
-                src="https://i.pinimg.com/1200x/3e/28/81/3e2881cbd62c055013e0a3a6cb22d53a.jpg"
+                src="https://i.pinimg.com/1200x/3b/ca/29/3bca29a3d839c76e4010ea5917cb03fd.jpg"
                 alt=""
               />
             </>
@@ -39,8 +39,13 @@ const Post_6: Template_Page = {
           caption={
             <p>
               Photo by{" "}
-              <a href="https://unsplash.com/@pgonzalez_95">Pedro Gonzalez</a> on{" "}
-              <a href="https://unsplash.com/">Unsplash</a>
+              <a href="https://unsplash.com/@freys?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+                Freysteinn G. Jonsson
+              </a>{" "}
+              on{" "}
+              <a href="https://unsplash.com/photos/a-panda-bear-sitting-in-a-small-pool-of-water-P41YxK9weyo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+                Unsplash
+              </a>
             </p>
           }
         />
@@ -70,7 +75,9 @@ const Post_6: Template_Page = {
           }
         />
 
-        <p>為此，我們專門開設了「認識我們的國寶：大熊貓」的主題展，來幫助大家了解這些可愛的角色。</p>
+        <p>
+          為此，我們專門開設了「認識我們的國寶：大熊貓」的主題展，來幫助大家了解這些可愛的角色。
+        </p>
 
         <Fig
           art={
@@ -83,7 +90,10 @@ const Post_6: Template_Page = {
           }
         />
 
-        <p>此外，我們也會通過多種方式，人機互動、VR 體驗、多語種閱讀，以及手工課等，幫助大家了解更多有關大熊貓的知識，以及⋯⋯請原諒⋯⋯他們的私生活。</p>
+        <p>
+          此外，我們也會通過多種方式，人機互動、VR
+          體驗、多語種閱讀，以及手工課等，幫助大家了解更多有關大熊貓的知識，以及⋯⋯請原諒⋯⋯他們的私生活。
+        </p>
 
         <Fig
           art={
@@ -96,7 +106,9 @@ const Post_6: Template_Page = {
           }
         />
 
-        <p>我們希望，通過這種方式，不僅能夠讓大家了解到有關大熊貓的一些生物學知識，更能夠了解到，生態環境之於人類的重要性，以及（同樣重要的）：大熊貓何以能夠成為各國之間和平的使者。要知道，我們的世界充滿了衝突，其中有一些確實可以並且曾經被可愛所感化的；但何以感化？這也是我們在主題展中所說的。</p>
+        <p>
+          我們希望，通過這種方式，不僅能夠讓大家了解到有關大熊貓的一些生物學知識，更能夠了解到，生態環境之於人類的重要性，以及（同樣重要的）：大熊貓何以能夠成為各國之間和平的使者。要知道，我們的世界充滿了衝突，其中有一些確實可以並且曾經被可愛所感化的；但何以感化？這也是我們在主題展中所說的。
+        </p>
       </Sec>
 
       <Sec>
@@ -104,7 +116,9 @@ const Post_6: Template_Page = {
 
         <h3>看點 1. 中華非遺皮影演出课</h3>
 
-        <p>利用中華非遺皮影戲的方式來傳播大熊貓保育的故事，不僅能夠讓觀眾在享受藝術表演的同時了解大熊貓保護的重要性，還能夠傳承和弘揚中國的非物質文化遺產。</p>
+        <p>
+          利用中華非遺皮影戲的方式來傳播大熊貓保育的故事，不僅能夠讓觀眾在享受藝術表演的同時了解大熊貓保護的重要性，還能夠傳承和弘揚中國的非物質文化遺產。
+        </p>
 
         <Fig
           art={
@@ -121,9 +135,9 @@ const Post_6: Template_Page = {
       <Sec>
         <h3>看點 2. 熊貓主題講座</h3>
 
-        <p>導師到校分享大熊貓百科、濒危物種保育、生態環境保護、自然科普等專题內容</p>
-
-        
+        <p>
+          導師到校分享大熊貓百科、濒危物種保育、生態環境保護、自然科普等專题內容
+        </p>
 
         <Fig
           art={
@@ -140,9 +154,9 @@ const Post_6: Template_Page = {
       <Sec>
         <h3>看點 3. 中華非遺手工课</h3>
 
-        <p>動手體驗來自國寶故鄕四川省的中華非遺手工-一皮影、織造術、造紙術燈彩等，感受中國人的造物智慧。</p>
-
-        
+        <p>
+          動手體驗來自國寶故鄕四川省的中華非遺手工-一皮影、織造術、造紙術燈彩等，感受中國人的造物智慧。
+        </p>
 
         <Fig
           art={
@@ -159,9 +173,9 @@ const Post_6: Template_Page = {
       <Sec>
         <h3>看點 4. 學校訂製款NFT紀念徽章</h3>
 
-        <p>為學校訂製熊貓主題專屬紀念徽章限量發行，有收藏價值，開拓同學們的科技視野。</p>
-
-        
+        <p>
+          為學校訂製熊貓主題專屬紀念徽章限量發行，有收藏價值，開拓同學們的科技視野。
+        </p>
 
         <Fig
           art={
@@ -178,9 +192,9 @@ const Post_6: Template_Page = {
       <Sec>
         <h3>看點 5. 熊貓主題閱讀角</h3>
 
-        <p>「熊貓主題閱讀角」展出主題圖書文創等產品，學生還可以與大熊貓玩偶合影，留下精彩互動瞬間。</p>
-
-        
+        <p>
+          「熊貓主題閱讀角」展出主題圖書文創等產品，學生還可以與大熊貓玩偶合影，留下精彩互動瞬間。
+        </p>
 
         <Fig
           art={
@@ -193,7 +207,9 @@ const Post_6: Template_Page = {
           }
         />
 
-        <p>我們能夠配合學校主题提供特別訂製服務，如果您對此感興趣，可以通過以下方式聯繫我們。</p>
+        <p>
+          我們能夠配合學校主题提供特別訂製服務，如果您對此感興趣，可以通過以下方式聯繫我們。
+        </p>
       </Sec>
 
       <ContactCallout
