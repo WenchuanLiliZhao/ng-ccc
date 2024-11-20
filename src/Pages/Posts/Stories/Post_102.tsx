5716622e81fbd09569033a363d4c54629cb9a168
cynthiatony <160562491+Tonysuep@users.noreply.github.com>
2024-11-20 13:26:29 +0800

    新增页面
    2014年11月20日新增内容

---
+++ src/Pages/Posts/Stories/Post_102.tsx
@@ -0,0 +1,36 @@
+import Sec from "../../_Templates/Layouts/InArticle/_ArticleSection";
+import Template_Page from "../../_Templates/Template_Page";
+import Authors from "../../Authors/_Authors";
+
+const Post_102:Template_Page = {
+    info: {
+        title: "最新的活动",
+        key: "post_102",
+        brief: "简介",
+        latest_update: [2024,11,20],
+        cover: "https://i.pinimg.com/1200x/c3/cc/16/c3cc1621a9627c1fdf7c4bdb64c460d5.jpg",
+        label: undefined,
+        front: undefined,
+        showHeader: undefined,
+        title_display: undefined,
+        authors: [
+            {
+              data: Authors.Author_Suep,
+            },
+          ],
+        keywords: ["关键字"]
+    },
+    content: (
+    <>
+        <Sec size="large">
+            <p>
+               fsdl;jfkkjgjjsfjklsajfkl加快了的就是风口浪尖上的浪费精力时间 案件使得开发商和老师弗兰克教授看发射的火箭发货了案件看来大家还是废话说多了会宽松的环境开发和开始了架空历史交锋手机开发和
+               士大夫十分阿达安抚是的 
+            </p>
+        </Sec>
+    </>
+
+    )
+}
+
+export default Post_102;
